46f25267739dd01ea9f00ceb01d513dc6fd7996d
JSRMario <juanmario@ciencias.unam.mx>
2025-06-26 19:53:30 -0600

    feat: agrega los primeros 2 problemas

---
+++ warmUp/SalesByMatch/Solution.java
@@ -0,0 +1,74 @@
+package hackerRank.warmUp.SalesByMatch;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        
+        if(n==0 || ar == null){
+            return 0;
+        }
+        
+        int pares = 0;
+        
+        Collections.sort(ar);
+        
+        int current = -1;
+        int count = 0;
+        
+        for(Integer sock : ar){
+            if(sock != current){
+                pares += count / 2;
+                current = sock;
+                count = 0;
+            }
+            count +=1;
+        }
+        pares += count / 2;
+        return pares;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
